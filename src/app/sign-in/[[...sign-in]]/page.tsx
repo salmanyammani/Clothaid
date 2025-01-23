a87b3f81428aa7d50f04743e572166c71ce4131d
salmanyammani <salman.yammani@gmail.com>
2025-01-23 10:09:20 +0500

    fixed login/signup ui

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,10 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+    <div className="flex min-h-svh  items-center justify-center bg-muted p-6 pt-8 md:p-10">
+      {/* <div className="w-full mx-auto max-w-sm md:max-w-3xl"> */}
         <SignIn />
-      </div>
+      {/* </div> */}
     </div>
   )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 export default function Page() {
     return (
-        <div className="">
-            <Image src={"/auth/banner.jpg"} width={900} height={900} alt='banner' className='h-full w-full object-contain'/>
-            <div className="absolute top-16 right-5">
+        <div className="flex min-h-svh  items-center justify-center bg-muted p-6 pt-8 md:p-20">
+            {/* <Image src={"/auth/banner.jpg"} width={900} height={900} alt='banner' className='h-full w-full object-contain'/> */}
+            {/* <div className="absolute top-16 right-5"> */}
             <SignUp />
-            </div>
+            {/* </div> */}
         </div>
     )
 }
